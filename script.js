441e60e9d616678fd16726a01838bccde678f962
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-01-11 12:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
+	if (!s1 || !s2) {
+    return '';
+  }
+
+  return s1[0] + s2[0];
 }
 
-// Do not change the code below.
+
+// Do not change the code belo.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
